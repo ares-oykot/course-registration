aae28f0692926022a30dc73f06d864ec18b55963
ares-oykot <soykotislam131415@gmail.com>
2023-09-16 13:43:14 +0600

    responsive web page for Mobile-L-425px

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -50,36 +50,36 @@ const Cards = () => {
   };
   return (
     <div className="">
-      <h1 className="text-center text-2xl lg:text-3xl font-bold mt-4">
+      <h1 className="text-center md:text-2xl lg:text-3xl font-bold mt-2 md:mt-4">
         Course Registration
       </h1>
-      <div className="flex gap-5 mt-3 lg:mt-8">
-        <div className="grid grid-cols-3 gap-2 lg:gap-3  w-9/12">
+      <div className="flex gap-1 md:gap-5 mt-2 md:mt-3 lg:mt-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-2 lg:gap-3  w-9/12">
           {cards.map((card, idx) => (
             <div className="" key={idx}>
-              <div className=" p-2 lg:p-4 rounded-md bg-white h-full flex flex-col justify-around">
+              <div className="p-1 md:p-2 lg:p-4 rounded-md bg-white h-full flex flex-col justify-around">
                 <div className="">
                   <img className="w-full" src={card.cover} alt="" />
                 </div>
-                <h3 className="text-[10px] lg:text-lg font-semibold  lg:pt-2">{card.title}</h3>
-                <p className=" text-[8px] lg:text-sm text-[#1C1B1B99]">
+                <h3 className="text-[8px] md:text-[10px] lg:text-lg font-semibold  lg:pt-2">{card.title}</h3>
+                <p className="text-[6px] md:text-[8px] lg:text-sm text-[#1C1B1B99]">
                   <span>{card.description}</span>
                 </p>
                 <div className="">
                   <div className="flex lg:my-3">
-                    <div className="flex items-center">
-                      <img className="w-3 h-3 lg:w-4 lg:h-4" src={Dollar} alt="" />
-                      <span className="text-[9px] lg:text-base"> Price : {card.price}</span>
+                    <div className="flex md:items-center">
+                      <img className="h-2 w-2 md:w-3 md:h-3 lg:w-4 lg:h-4" src={Dollar} alt="" />
+                      <span className="text-[7px] md:text-[9px] lg:text-base"> Price : {card.price}</span>
                     </div>
-                    <div className="flex items-center ml-2 lg:ml-3">
-                      <img className="w-4 h-4 mr-1" src={Frame} alt="" />
-                      <span className="text-[9px] lg:text-base">Credit : {card.credit} hr</span>
+                    <div className="flex md:items-center ml-2 lg:ml-3">
+                      <img className="h-2 w-2 md:w-4 md:h-4 mr-1" src={Frame} alt="" />
+                      <span className="text-[7px] md:text-[9px] lg:text-base">Credit : {card.credit} hr</span>
                     </div>
                   </div>
                 </div>
                 <button
                   onClick={() => handleSelect(card)}
-                  className="w-full text-white py-1 lg:py-2 text-[8px] lg:text-base rounded lg:rounded-md mt-1 lg:mt-2 bg-[#2F80ED] hover:bg-[#3445ff]"
+                  className="w-full text-white py-[2px] md:py-1 lg:py-2 text-[7px] md:text-[8px] lg:text-base rounded-sm  md:rounded lg:rounded-md md:mt-1 lg:mt-2 bg-[#2F80ED] hover:bg-[#3445ff]"
                 >
                   Select
                 </button>
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,22 +1,22 @@
 const Cart = ({ selectCard, totalCredit, remaining, totalPrice }) => {
   return (
     <>
-      <div className="w-1/4 p-2 lg:p-4 bg-white rounded-md">
-        <h4 className="text-xs lg:text-lg font-bold text-[#2F80ED] border-b lg:border-b-2 pb-2 lg:pb-3 mb-2 lg:mb-4">
+      <div className="w-1/4 p-1 md:p-2 lg:p-4 bg-white rounded md:rounded-md">
+        <h4 className="text-[6px] md:text-xs lg:text-lg font-bold text-[#2F80ED] border-b lg:border-b-2 pb-1 md:pb-2 lg:pb-3 mb-1 md:mb-2 lg:mb-4">
           Credit Hour Remaining {remaining} hr
         </h4>
-        <h4 className="text-[10px] lg:text-lg font-bold">Course Name</h4>
-        <div className="mt-2 lg:mt-4">
+        <h4 className="text-[6px] md:text-[10px] lg:text-lg font-bold">Course Name</h4>
+        <div className="mb-1 md:mb-0 mt-1 md:mt-2 lg:mt-4">
           {selectCard.map((card, idx) => (
             <ol key={idx} className="">
-              <li className="text-[8px] lg:text-base text-black bg-slate-200 mb-2 rounded px-1 lg:px-2">
+              <li className="text-[5px] md:text-[8px] lg:text-base text-black bg-slate-200 py-[1py] md:py-0 mb-[2px] md:mb-1 lg:mb-2 rounded-sm md:rounded px-1 lg:px-2">
                 {idx + 1} : {card.title}
               </li>
             </ol>
           ))}
         </div>
-        <h5 className="border-y lg:border-y-2 py-2 lg:py-4 lg:mt-5 mb-2 lg:mb-4 text-[10px] lg:text-base font-medium">Total Credit Hour : {totalCredit}</h5>
-        <h5 className="text-[10px] lg:text-base font-bold">Total Price : {totalPrice} USD </h5>
+        <h5 className="border-y lg:border-y-2 py-1 md:py-2 lg:py-4 lg:mt-5 mb-1 md:mb-2 lg:mb-4 text-[5px] md:text-[10px] lg:text-base font-medium">Total Credit Hour : {totalCredit}</h5>
+        <h5 className="text-[6px] md:text-[10px] lg:text-base font-bold">Total Price : {totalPrice} USD </h5>
       </div>
     </>
   );
